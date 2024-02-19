61d6bf4b22eb43664b0c9a6cc7806a8d09687061
svemargit <svejdam@icloud.com>
2024-02-19 19:03:35 +0100

    form setup

---
+++ src/assets/php/send-form.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $option = $_POST['option'];
+    $message = $_POST['message'];
+    $to = 'svejdam@icloud.com';
+
+    // Email subject
+    $subject = 'SoftIQ zpráva';
+
+    // Email message
+    $email_message = "Odeslal: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Typ webu: $option\n";
+    $email_message .= "Zpráva:\n$message\n";
+
+    // Email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    // Send the email
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "<script>alert('Děkuji za zprávu, co nejdříve se Vám ozvu zpět.');</script>";
+    } else {
+        echo "<script>alert('Bohužel se to nepovedlo a email se neodeslal. Napište mi na svejdam@icloud.com');</script>";
+    }
+}
+?>
